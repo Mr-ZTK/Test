712cf74634496fe6fc510e52439ca3778a53e8f1
Mr-ZTK <43575597+Mr-ZTK@users.noreply.github.com>
2019-11-24 10:05:43 +0800

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Test_Partial.Models;
+
+namespace Test_Partial.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<MenuItem> list = new List<MenuItem>();
+
+            list.Add(new MenuItem { Link = "/Home/Index", LinkName = "Home" });
+            list.Add(new MenuItem { Link = "/Home/About", LinkName = "Login" });
+            list.Add(new MenuItem { Link = "/Home/Contact", LinkName = "Register" });
+
+            return View(list);
+        }
+
+        public ActionResult SideMenu()
+        {
+            List<MenuItem> list = new List<MenuItem>();
+
+            list.Add(new MenuItem { Link = "/Home/Index", LinkName = "Home" });
+            list.Add(new MenuItem { Link = "/Home/About", LinkName = "Login" });
+            list.Add(new MenuItem { Link = "/Home/Contact", LinkName = "Register" });
+
+
+            return PartialView("SideMenu", list);
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MenuItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Test_Partial.Models
+{
+    public class MenuItem
+    {
+        public string LinkName { get; set; }
+        public string Link { get; set; }
+    }
+}
